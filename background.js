57dc5b68faa482ded157f2a89f1511d729439f48
kenta7777 <k.hasegw7@gmail.com>
2020-12-16 00:51:22 +0900

    Add a function which gets some elements randomly from read later array

--- background.js
+++ background.js
@@ -12,18 +12,50 @@ var opt = {
 // set up addListener for notifies read later contents periodically
 chrome.alarms.onAlarm.addListener(function(alarm){
     console.log('test: read-later-notification')
+    
+    // get all bookmark tree
     chrome.bookmarks.getTree(function(bookmark){
-        console.log(bookmark)
-        var root = bookmark[0]['children'];
-        console.log(root)
+        var root = bookmark[0]['children']
         var bookMarkBar = root[0]['children']
-        console.log(bookMarkBar)
         var readLater = bookMarkBar[11]['children']
-        console.log(readLater)
 
+        // get random elements from readLater 
+        var selectedReadLater = getRandomIntArray(readLater, 3)
+        
+        selectedReadLater.forEach(function(item, index, array) {
+            console.log(index, item)
+        })
+        
+        //TODO: get the title and URL from them and set as notification
+
+        //TODO: send notificaion
     })
     // chrome.notifications.create('notify1', opt, function(id) { console.log("Last error:", chrome.runtime.lastError) } )
     // chrome.notifications.create('notify2', opt, function(id) { console.log("Last error:", chrome.runtime.lastError) } )
     // chrome.notifications.create('notify3', opt, function(id) { console.log("Last error:", chrome.runtime.lastError) } )
     //chrome.notifications.create('notify1', opt, function(id) { } )
 });
+
+// get 10 elements from argument
+function getRandomIntArray(array, number) {
+    if (number == 0) {
+        return []
+    }
+    
+    var randomIntArray = []
+    const max = array.length - 1
+    
+    for (let index = 0; index < number ;index++) {
+        var randomInt = getRandomInt(0, max)
+        randomIntArray.push(array[randomInt])
+    }
+    
+    return randomIntArray
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    
+    return Math.floor(Math.random() * (max - min) + min); 
+}
