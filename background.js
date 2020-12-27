631d5a36e2b2322f8d21e89d796e08771ced3ff7
Kenta <k.hasegw7@gmail.com>
2020-12-27 20:36:06 +0900

    Enable to specify bookmark directory (#5)

--- background.js
+++ background.js
@@ -3,58 +3,61 @@ chrome.alarms.create('read later notifier', { delayInMinutes : 1, periodInMinute
 
 // set up addListener for notifies read later contents periodically
 chrome.alarms.onAlarm.addListener(function(alarm){
-    console.log('test: read-later-notification')
     
     // get all bookmark tree
     chrome.bookmarks.getTree(function(bookmark){
+        // TODO: enable to select directory name and content item number in setting menu(#3)
+        var selectedDirectoryName = "read_later"
+        var selectedItemNumber = 3
         var root = bookmark[0]['children']
         var bookMarkBar = root[0]['children']
-        var readLater = bookMarkBar[11]['children']
 
-        // get random elements from readLater 
-        var selectedReadLater = getRandomIntArray(readLater, 3)
-        
-        selectedReadLater.forEach(function(item, index, array) {
-            console.log(index, item)
-            console.log(item.id)
-            console.log(item.title)
-            console.log(item.url)
-
-            var opt = {
-                type: 'list',
-                title: item.title,
-                message: item.url,
-                priority: 1,
-                items: [{ title: item.url, message: ''}],
-                iconUrl:'./my-image.png'
-            };
-
-            // if clicked, open web page with the specified URL
-            //TODO: fix a bug which shows three pages without clicking notification
-            chrome.notifications.create('index', opt, function(id) { 
-                chrome.notifications.onClicked.addListener(notificationClicked(item.url))
-                console.log("Last error:", chrome.runtime.lastError) 
-            } )
+        // get read later directory from book mark bar
+        var readLater = bookMarkBar.filter(function(element, index, array) {
+            return isChildrenArray(element) && element.title == selectedDirectoryName
         })
+
+        // get random read later contents from readLater 
+        // TODO: error handling(if n <= 2) when implementing setting menu(#3)
+        var selectedReadLater = getReadLaterContents(readLater[0]['children'], selectedItemNumber)
+        console.log(selectedReadLater)
+        
+        // selectedReadLater.forEach(function(item, index, array) {
+        //     var opt = {
+        //         type: 'list',
+        //         title: item.title,
+        //         message: item.url,
+        //         priority: 1,
+        //         items: [{ title: item.url, message: ''}],
+        //         iconUrl:'./my-image.png'
+        //     };
+
+        //     // if clicked, open web page with the specified URL
+        //     //TODO: fix a bug which shows three pages without clicking notification
+        //     chrome.notifications.create('index', opt, function(id) { 
+        //         chrome.notifications.onClicked.addListener(notificationClicked(item.url))
+        //         console.log("Last error:", chrome.runtime.lastError) 
+        //     } )
+        // })
         
     })
 });
 
-// get 10 elements from argument
-function getRandomIntArray(array, number) {
+function getReadLaterContents(array, number) {
+
     if (number == 0) {
         return []
     }
     
-    var randomIntArray = []
-    const max = array.length - 1
+    var selectedReadLaterContentsArray = []
+    const max = array.length
     
     for (let index = 0; index < number ;index++) {
         var randomInt = getRandomInt(0, max)
-        randomIntArray.push(array[randomInt])
+        selectedReadLaterContentsArray.push(array[randomInt])
     }
     
-    return randomIntArray
+    return selectedReadLaterContentsArray
 }
 
 function getRandomInt(min, max) {
@@ -62,6 +65,10 @@ function getRandomInt(min, max) {
     max = Math.floor(max);
     
     return Math.floor(Math.random() * (max - min) + min); 
+}
+
+function isChildrenArray(elem) {
+    return 'children' in elem
 }
 
 function notificationClicked(url) {
