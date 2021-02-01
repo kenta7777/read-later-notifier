a533e827cdd225978bdf0bcaf181218849c2012c
kenta7777 <k.hasegw7@gmail.com>
2021-02-02 00:31:40 +0900

    Fix the variable naming and comment

--- background.js
+++ background.js
@@ -8,18 +8,18 @@ chrome.alarms.onAlarm.addListener(function(alarm){
     chrome.bookmarks.getTree(function(bookmark){
         // TODO: enable to select directory name and content item number in setting menu(#3)
         var selectedDirectoryName = "read_later"
-        var selectedItemNumber = 3
+        var selectedContentCount = 3
         var root = bookmark[0]['children']
         var bookMarkBar = root[0]['children']
 
         // get read later directory from book mark bar
-        var readLater = bookMarkBar.filter(function(element, index, array) {
-            return isChildrenArray(element) && element.title == selectedDirectoryName
+        var readLater = bookMarkBar.filter(function(elem, index, array) {
+            return isChildrenArray(elem) && elem.title == selectedDirectoryName
         })
 
         // get random read later contents from readLater 
         // TODO: error handling(if n <= 2) when implementing setting menu(#3)
-        var selectedReadLater = getReadLaterContents(readLater[0]['children'], selectedItemNumber)
+        var selectedReadLater = getReadLaterContents(readLater[0]['children'], selectedContentCount)
         console.log(selectedReadLater)
         
         selectedReadLater.forEach(function(item, index, array) {
@@ -35,15 +35,15 @@ chrome.alarms.onAlarm.addListener(function(alarm){
     })
 });
 
-function getReadLaterContents(contentsArray, contentsTotalCount) {
-    if (contentsTotalCount == 0) {
+function getReadLaterContents(contentsArray, selectedContentCount) {
+    if (selectedContentCount == 0) {
         return []
     }
     
     var selectedContentsArray = []
-    const max = contentsArray.length
+    const contentTotalCount = contentsArray.length
 
-    var randomIndexesArray = getRandomIndexes(max, contentsTotalCount)
+    var randomIndexesArray = getRandomIndexes(contentTotalCount, selectedContentCount)
     randomIndexesArray.forEach( elem => {
         selectedContentsArray.push(contentsArray[elem])
     })
@@ -58,14 +58,14 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min); 
 }
 
-// get an array that holds selected number of index for contents array
-// to avoid the duplicate of contents
-function getRandomIndexes(max, selectedItemNumber) {
+// get selected number of indexes without duplication 
+// to avoid showing same contents
+function getRandomIndexes(contentTotalCount, selectedContentCount) {
     var randomIndexesArray = []
     
-    for (let index = 0; index < selectedItemNumber; index++) { 
+    for (let index = 0; index < selectedContentCount; index++) { 
         while(true) {
-            var randomInt = getRandomInt(0, max)
+            var randomInt = getRandomInt(0, contentTotalCount)
             if(!randomIndexesArray.includes(randomInt)) {
                 randomIndexesArray.push(randomInt)
                 break
