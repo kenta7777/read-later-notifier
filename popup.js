4efef4cf9f6e805dddfa937f4b44016695b33693
kenta7777 <k.hasegw7@gmail.com>
2021-02-22 01:21:43 +0900

    Make selected folder text fields editable if they are tapped

--- popup.js
+++ popup.js
@@ -10,13 +10,17 @@
 // use DOMContentLoaded to wait for loading HTML 
 document.addEventListener("DOMContentLoaded", function(){
     var selected_folder = document.getElementById('selected_folder')
-    var selected_folder_button = document.getElementById('selected_folder_button');
+    var selected_folder_set_button = document.getElementById('selected_folder_button');
     
     // make text fields editable if they are tapped
+    selected_folder.addEventListener("click", function() {
+        console.log("selected folder text field is clicked!");
+        selected_folder.contentEditable = true;
+    });
 
     // make text fields non editable and inputted text is stored in chrome if the set button is tapped 
-    selected_folder_button.addEventListener("click", function() {
-        console.log("selected_folder has been clicked!");
+    selected_folder_set_button.addEventListener("click", function() {
+        console.log("selected folder set button is clicked!");
         selected_folder.contentEditable = false
         // store inputted folder name
     });
